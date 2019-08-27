e18620b9f9b76b2b582e62fbdda9e79bc772fd75
Konstantin Konovalov <7835507+ktwice@users.noreply.github.com>
2019-08-27 23:34:53 +0500

    Update Eisws.java

--- Eisws.java
+++ Eisws.java
@@ -12,52 +12,56 @@ import javax.xml.stream.*;
  * @author k2-adm
  */
 public class Eisws {
-    protected static final Charset UTF8 = Charset.forName("UTF-8");
-    protected static final String DOCS_WSS =
-        "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-";
-    protected static final SimpleDateFormat SDF_UTC =
-        new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-    static {
-        SDF_UTC.setTimeZone(TimeZone.getTimeZone("UTC"));
-    }
-    protected static Map<String,String> nss = new TreeMap<>();
-    protected static Map<String,String> services = new TreeMap<>();
 /**
  * Run requests and save results
  * @param args args[0] xml-file with requests
  */    
     public static void main(String[] args) {
+        final Map<String,String> nss = new TreeMap<>();
+        final Map<String,String> services = new TreeMap<>();
+        final String xname = args[0]; 
+        final String xprefix = xname.substring(1
+            + Math.max(xname.lastIndexOf("/"), xname.lastIndexOf("\\"))) + "-";
     try {
-        SOAPConnectionFactory cf = SOAPConnectionFactory.newInstance();
-        SOAPConnection connection = cf.createConnection();
-
-        XMLInputFactory xf = XMLInputFactory.newInstance();
-        XMLStreamReader xsr = xf.createXMLStreamReader(
-                new FileInputStream(args[0]));
+        final MessageFactory mf = MessageFactory.newInstance();
+        final SOAPConnectionFactory cf = SOAPConnectionFactory.newInstance();
+        final SOAPConnection connection = cf.createConnection();
+        final XMLInputFactory xf = XMLInputFactory.newInstance();
+        final XMLStreamReader xsr = xf.createXMLStreamReader(
+            new FileInputStream(xname));
         xsr.nextTag();
-        System.out.println(xsr.getLocalName()+" root element.");
-        while(xsr.nextTag() == xsr.START_ELEMENT) {
+        System.out.println(xname + " <" + xsr.getLocalName()+"> root element.");
+        while(xsr.nextTag() == XMLStreamReader.START_ELEMENT) {
             String tag = xsr.getLocalName();
             switch(tag) {
                 case "ns":
                     nss.put(xsr.getAttributeValue(null, "name")
-                            , xsr.getAttributeValue(null, "addr"));
-                    if(xsr.nextTag() == xsr.START_ELEMENT)
+                        , xsr.getAttributeValue(null, "addr"));
+                    if(xsr.nextTag() == XMLStreamReader.START_ELEMENT)
                         throw new Exception("tag=ns has unexpected child");
                     break;
                 case "service":
                     String addr = xsr.getAttributeValue(null, "addr");
                     String name = xsr.getAttributeValue(null, "name");
-                    if(name==null) name = addr.substring(addr.lastIndexOf("/")+1);
+                    if(name==null) name = addr.substring(1 + addr.lastIndexOf("/"));
                     services.put(name, addr);
-                    if(xsr.nextTag() == xsr.START_ELEMENT)
+                    if(xsr.nextTag() == XMLStreamReader.START_ELEMENT)
                         throw new Exception("tag=service has unexpected child");
                     break;
                 case "out":
-                    String service = xsr.getAttributeValue(null, "service");
-                    String fname = xsr.getAttributeValue(null, "fname");
-                    SOAPMessage rq = wsRequest(xsr);
-                    xmlOut(fname, rq, connection.call(rq, services.get(service)));
+    String service = xsr.getAttributeValue(null, "service");
+    String fname = xsr.getAttributeValue(null, "fname");
+    SOAPMessage m = newMessage(mf);
+    String[] es = xsr.getAttributeValue(null, "operation").split(":");
+    SOAPElement e = es.length>1
+        ? m.getSOAPBody().addChildElement(es[1], es[0], nss.get(es[0]))
+        : m.getSOAPBody().addChildElement(es[0]);
+    while(xsr.nextTag() == XMLStreamReader.START_ELEMENT)
+        e.addChildElement(xsr.getAttributeValue(null, "name"))
+            .addTextNode(xsr.getElementText());
+    m.saveChanges();
+    xmlOut(xprefix + fname + ".xml", m
+        , connection.call(m, services.get(service)));
                     break;
                 default: throw new Exception("unknown tag="+tag+" in root");
             }
@@ -71,18 +75,20 @@ public class Eisws {
     }
 /**
  * Save request and response to xml-file
- * @param name xml-filename base-part
+ * @param fname xml-filename 
  * @param request SOAP request-message
  * @param respond SOAP respond-message
  */    
-    protected static void xmlOut(String name
+    protected static void xmlOut(String fname
     , SOAPMessage request
     , SOAPMessage respond) throws SOAPException, IOException {
-        String fname = "eis-" + name + ".xml";
+        final Charset UTF8 = Charset.forName("UTF-8");
         System.out.print(fname);
         try (OutputStream xml = new FileOutputStream(fname)) {
             xml.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".getBytes(UTF8));
             xml.write("\n<out><request>\n".getBytes(UTF8));
+            request.getSOAPHeader().detachNode(); // without access codes
+            request.saveChanges();
             request.writeTo(xml);
             xml.write("\n</request><respond>\n".getBytes(UTF8));
             respond.writeTo(xml);
@@ -91,57 +97,36 @@ public class Eisws {
         System.out.println(" completed.");
     }
 /**
- * Building SOAP request-message
- * @param xsr XMLStreamReader (out-element)
- * @return completed request-message
- */ 
-    protected static SOAPMessage wsRequest(XMLStreamReader xsr
-        ) throws SOAPException, IOException, XMLStreamException, Exception {
-        MessageFactory mf = MessageFactory.newInstance();
-        SOAPMessage m = mf.createMessage();
-        String eiswsu = System.getenv("eiswsu");
-        if(eiswsu==null) {
-            System.out.println("eiswsu environment variable not exists!");
-            System.exit(1);
-        }
-        wsHeader(m, eiswsu, System.getenv("eiswsp"));
-        String[] oper = xsr.getAttributeValue(null, "operation").split(":");
-        SOAPElement operation = oper.length>1
-        ? m.getSOAPBody().addChildElement(oper[1], oper[0], nss.get(oper[0]))
-        : m.getSOAPBody().addChildElement(oper[0]);
-        while(xsr.nextTag() == xsr.START_ELEMENT) {
-            String name = xsr.getAttributeValue(null, "name");
-            String text = xsr.getElementText();
-            operation.addChildElement(name).addTextNode(text);
-        }
-        m.saveChanges();
-        return m;
-    }
-/**
  * Completely fill header for just-created SOAP request-message
- * @param message
- * @param username
- * @param password
+ * @param mf SOAPMessage factory
+ * @return just-created SOAP request-message
  */    
-    static protected void wsHeader(SOAPMessage message,
-        final String username, final String password) throws SOAPException {
-        SOAPHeader header = message.getSOAPHeader();
+    static protected SOAPMessage newMessage(MessageFactory mf)
+        throws SOAPException, Exception {
+        final String DOCS_WSS =
+            "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-";
+        final SimpleDateFormat SDF_UTC =
+            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        SDF_UTC.setTimeZone(TimeZone.getTimeZone("UTC"));
+
         Date d = new Date();
+        SOAPMessage message = mf.createMessage();
+        SOAPHeader header = message.getSOAPHeader();
         header.addNamespaceDeclaration("wsse"
-                , DOCS_WSS + "wssecurity-secext-1.0.xsd");
+            , DOCS_WSS + "wssecurity-secext-1.0.xsd");
         header.addNamespaceDeclaration("wsu"
-                , DOCS_WSS + "wssecurity-utility-1.0.xsd");
+            , DOCS_WSS + "wssecurity-utility-1.0.xsd");
         SOAPElement security = header.addChildElement("Security", "wsse");
 
         SOAPElement ut = security.addChildElement("UsernameToken", "wsse");
-        ut.addChildElement("Username", "wsse").setTextContent(username);
+        ut.addChildElement("Username", "wsse").setTextContent(getEnv("eiswsu"));
         SOAPElement p = ut.addChildElement("Password", "wsse");
         p.addAttribute(new QName("Type")
-                , DOCS_WSS + "username-token-profile-1.0#PasswordText");
-        p.setTextContent(password);
+            , DOCS_WSS + "username-token-profile-1.0#PasswordText");
+        p.setTextContent(getEnv("eiswsp"));
         SOAPElement n = ut.addChildElement("Nonce", "wsse");
         n.addAttribute(new QName("EncodingType")
-                , DOCS_WSS + "soap-message-security-1.0#Base64Binary");
+            , DOCS_WSS + "soap-message-security-1.0#Base64Binary");
         n.setTextContent(getNonce());
         ut.addChildElement("Created", "wsu").setTextContent(SDF_UTC.format(d));
         
@@ -150,6 +135,7 @@ public class Eisws {
         ts.addChildElement("Created", "wsu").setTextContent(SDF_UTC.format(d));
         d = new Date(d.getTime() + 60000); // 0ne minute!
         ts.addChildElement("Expires", "wsu").setTextContent(SDF_UTC.format(d));
+        return message;
     }
 /**
  * Random value for Nonce-element
@@ -159,6 +145,11 @@ public class Eisws {
         SecureRandom sr = new SecureRandom();
         sr.setSeed(System.currentTimeMillis());
         return Base64.getEncoder().encodeToString(sr.generateSeed(16));
+    }
+    static protected String getEnv(String ename) throws Exception {
+        String s = System.getenv(ename);
+        if(s != null) return s;
+        throw new Exception(ename + " environment variable not exists!");
     }
 
 }
